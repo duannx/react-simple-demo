f5da9e51b4dc0414cdf53b0bb0e20db912cb833f
duandz <duan.nguyen@electrolux.com>
2024-08-16 17:59:22 +0700

    Add reconciliation example

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,3 +1,6 @@
-export default function Item({ number }) {
-  return  <p>Item: {number}</p>
+export default function Item({ number, useSpan }) {
+  if (useSpan) {
+    return <span>Tag span, Item: {number}</span>;
+  }
+  return <p>Tag p, Item: {number}</p>;
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
 import Item from "./Item";
 
 export default function List() {
+  const [useSpan, setUseSpan] = useState(false);
+  const [useSection, setUseSection] = useState(false);
+  if (useSection) {
+    return (
+      <section>
+        <Item useSpan={false} number={1}></Item>
+        <Item useSpan={false} number={2}></Item>
+        <Item useSpan={useSpan} number={3}></Item>
+        <p></p>
+        <button onClick={() => setUseSpan(!useSpan)}> Toggle Item 3</button>
+        <p></p>
+        <button onClick={() => setUseSection(!useSection)}>
+          {" "}
+          Toggle parent div
+        </button>
+      </section>
+    );
+  }
   return (
     <div>
-      <Item number={1}></Item>
-      <Item number={2}></Item>
-      <Item number={3}></Item>
+      <Item useSpan={false} number={1}></Item>
+      <Item useSpan={false} number={2}></Item>
+      <Item useSpan={useSpan} number={3}></Item>
+      <p></p>
+      <button onClick={() => setUseSpan(!useSpan)}> Toggle Item 3</button>
+      <p></p>
+      <button onClick={() => setUseSection(!useSection)}>
+        {" "}
+        Toggle parent div
+      </button>
     </div>
   );
 }
