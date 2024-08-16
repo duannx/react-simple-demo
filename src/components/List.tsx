a674e03f77b08417ffad6c9c5337bd95379edfc1
duandz <duan.nguyen@electrolux.com>
2024-08-16 18:31:58 +0700

    add reconciliation key

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,36 +2,25 @@ import { useState } from "react";
 import Item from "./Item";
 
 export default function List() {
-  const [useSpan, setUseSpan] = useState(false);
-  const [useSection, setUseSection] = useState(false);
-  if (useSection) {
-    return (
-      <section>
-        <Item useSpan={false} number={1}></Item>
-        <Item useSpan={false} number={2}></Item>
-        <Item useSpan={useSpan} number={3}></Item>
-        <p></p>
-        <button onClick={() => setUseSpan(!useSpan)}> Toggle Item 3</button>
-        <p></p>
-        <button onClick={() => setUseSection(!useSection)}>
-          {" "}
-          Toggle parent div
-        </button>
-      </section>
-    );
+  const [items, setItems] = useState([
+    { id: 1, useSpan: false },
+    { id: 2, useSpan: false },
+    { id: 3, useSpan: true },
+  ]);
+
+  function reverse() {
+    const newItems = [...items];
+    newItems.reverse();
+    setItems(newItems);
   }
   return (
     <div>
-      <Item useSpan={false} number={1}></Item>
-      <Item useSpan={false} number={2}></Item>
-      <Item useSpan={useSpan} number={3}></Item>
+      {items.map((item) => {
+        return <Item key={item.id} {...item} number={item.id}></Item>;
+      })}
       <p></p>
-      <button onClick={() => setUseSpan(!useSpan)}> Toggle Item 3</button>
+      <button onClick={reverse}>Reverse</button>
       <p></p>
-      <button onClick={() => setUseSection(!useSection)}>
-        {" "}
-        Toggle parent div
-      </button>
     </div>
   );
 }
